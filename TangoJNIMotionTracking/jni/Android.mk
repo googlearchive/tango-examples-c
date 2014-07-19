f74a208f62f579e44262d4400b820b1d18afa670
Cheng Yang <chengyangorange@gmail.com>
2014-07-18 18:24:40 -0700

    Modify the activity to adjust to same structure of other JNI examples, modify the android.mk file to using new names.

--- TangoJNIMotionTracking/jni/Android.mk
+++ TangoJNIMotionTracking/jni/Android.mk
@@ -7,9 +7,9 @@ LOCAL_EXPORT_C_INCLUDES := ../TangoDevUpdate-0618/include
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := hello-tango-jni
+LOCAL_MODULE := libtango-native-jni
 LOCAL_SHARED_LIBRARIES := libtango-prebuilt 
-LOCAL_SRC_FILES := hello-tango-jni.cpp
+LOCAL_SRC_FILES := tango_motion_tracking.cpp
 LOCAL_C_INCLUDES := ../glm/glm
 LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog -lGLESv2
 LOCAL_ARM_MODE := arm
