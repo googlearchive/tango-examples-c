dc18820506d2f9c933cf7fb915b0a817ffdd7581
Cheng Yang <chengyangorange@gmail.com>
2014-07-22 15:39:43 -0700

    Add v2.0 library and header file, change the mk file to load new library.

--- TangoJNIMotionTracking/jni/Android.mk
+++ TangoJNIMotionTracking/jni/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libtango-prebuilt
-LOCAL_SRC_FILES := ../../TangoDevUpdate-0618/libs/libtango_api.so
+LOCAL_SRC_FILES := ../../TangoDevUpdate-0618/libs/libtango_service_api.so
 LOCAL_EXPORT_C_INCLUDES := ../TangoDevUpdate-0618/include
 include $(PREBUILT_SHARED_LIBRARY)
 
