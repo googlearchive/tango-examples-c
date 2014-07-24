24fbb3a7fcd3237e015d69e3d3455a719ef7c504
Jason Guo <xuguo@google.com>
2014-07-24 15:50:35 -0700

    - clean up unused libs. - remove config file alloc process (too complex for getting started example)

--- HelloTangoJNI/jni/Android.mk
+++ HelloTangoJNI/jni/Android.mk
@@ -4,5 +4,5 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := libtango-native-jni
 LOCAL_C_INCLUDES := ./tango_service/include/
 LOCAL_SRC_FILES := tango_native.cpp
-LOCAL_LDLIBS    := -llog -lGLESv2 -ltango_service_api
+LOCAL_LDLIBS    := -llog -ltango_service_api
 include $(BUILD_SHARED_LIBRARY)
--- HelloTangoJNI/jni/tango_native.cpp
+++ HelloTangoJNI/jni/tango_native.cpp
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include <jni.h>
 #include <android/log.h>
 #include <tango_client_api.h>
@@ -8,44 +7,20 @@
 #define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
 
 bool SetupTango() {
-  TangoConfig* config;
   if (TangoService_initialize() != 0) {
     LOGE("TangoService_initialize(): Failed\n");
     return -1;
   }
-  
-  // Allocate a TangoConfig object.
-  if ((config = TangoConfig_alloc()) == NULL) {
-    LOGE("TangoService_allocConfig(): Failed\n");
-    return -1;
-  }
-  
-  // Get the default TangoConfig.
-  if (TangoService_getConfig(TANGO_CONFIG_DEFAULT, config) != 0) {
-    LOGE("TangoService_getConfig(): Failed\n");
-    return -1;
-  }
-  
-  // Set a parameter in TangoConfig.
-  if (TangoConfig_setBool(config, "disable_opengl", true) != 0) {
-    LOGE("TangoConfig_setBool(\"disable_opengl\", true): Failed\n");
-    return -1;
-  }
-  
-  // Report the current TangoConfig.
-  LOGI("TangoConfig:\n%s\n", TangoConfig_toString(config));
-  
-  // Lock in this configuration.
-  if (TangoService_lockConfig(config) != 0) {
-    LOGE("TangoService_lockConfig(): Failed\n");
-    return -1;
-  }
 
   // Connect to the Tango Service.
-  TangoService_connect();
-
-  LOGI("HELLO TANGO, SERVICE CONNECTED!\n");
-  
+  if (TangoService_connect() != 0) {
+    LOGE("TangoService_connect(): Failed\n");
+    return -1;
+  }
+  else
+  {
+    LOGI("HELLO TANGO, SERVICE CONNECTED!\n");
+  }
   return true;
 }
 
