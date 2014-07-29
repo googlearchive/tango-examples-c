a780ecb45311d285a054d31ee195b8a424c2abde
Jason Guo <xuguo@google.com>
2014-07-29 16:53:50 -0700

    - change from linking system lib to linking local prebuilt lib.

--- HelloTangoJNI/jni/Android.mk
+++ HelloTangoJNI/jni/Android.mk
@@ -1,8 +1,14 @@
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := libtango-prebuilt
+LOCAL_SRC_FILES := ../../tango_service/libtango_service_api.so
+LOCAL_EXPORT_C_INCLUDES := ../tango_service/include
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE    := libtango-native-jni
-LOCAL_C_INCLUDES := ./tango_service/include/
+LOCAL_SHARED_LIBRARIES := libtango-prebuilt
 LOCAL_SRC_FILES := tango_native.c
-LOCAL_LDLIBS    := -llog -ltango_service_api
+LOCAL_LDLIBS    := -llog
 include $(BUILD_SHARED_LIBRARY)
