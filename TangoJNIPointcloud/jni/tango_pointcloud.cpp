4584dc9100ba413bcce23c66d3a2ad634425934f
Jason Guo <xuguo@google.com>
2014-07-30 15:41:13 -0700

    drawing points.

--- TangoJNIPointcloud/jni/tango_pointcloud.cpp
+++ TangoJNIPointcloud/jni/tango_pointcloud.cpp
@@ -4,6 +4,7 @@
 #include <GLES2/gl2.h>
 #include <GLES2/gl2ext.h>
 
+
 #include <tango-api/application-interface.h>
 #include <tango-api/hardware-control-interface.h>
 #include <tango-api/video-overlay-interface.h>
@@ -18,34 +19,53 @@
 #define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
 #define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
 
-static const char kVertexShader[] = "precision highp float;\n"
-"precision highp int;\n"
+static const char kVertexShader[] =
 "attribute vec4 vertex;\n"
-"uniform mat4 mvp"
-"attribute vec2 textureCoords;\n"
-"varying vec2 f_textureCoords;\n"
+"uniform mat4 mvp;\n"
+"varying vec4 v_normal;\n"
 "void main() {\n"
-"  f_textureCoords = textureCoords;\n"
+"  gl_PointSize = 10.0;\n"
 "  gl_Position = mvp*vertex;\n"
+"  v_normal = vertex;\n"
 "}\n";
 
 static const char kFragmentShader[] =
-"#extension GL_OES_EGL_image_external : require\n"
-"precision highp float;\n"
-"precision highp int;\n"
-"uniform samplerExternalOES texture;\n"
-"varying vec2 f_textureCoords;\n"
+"varying vec4 v_normal;\n"
 "void main() {\n"
-"  gl_FragColor = texture2D(texture, f_textureCoords);\n"
+"  gl_FragColor = vec4(v_normal);\n"
 "}\n";
 
-static const GLfloat kVertices[] = { -0.5, 0.5, 0.0, -0.5, -0.5, 0.0, 0.5, 0.5,
-  0.0, 0.5, -0.5, 0.0 };
+static const GLfloat kVertices[] = {
+  -0.5f, 0.5f, -0.5f,
+  0.5f, 0.5f, -0.5f,
+  -0.5f, -0.5f, -0.5f,
+  0.5f, -0.5f, -0.5f,
+  
+  -0.5f, 0.5f, 0.5f,
+  0.5f, 0.5f, 0.5f,
+  -0.5f, -0.5f, 0.5f,
+  0.5f, -0.5f, 0.5f};
 
-static const GLushort kIndices[] = { 0, 1, 2, 2, 1, 3 };
+static const GLushort kIndices[] = {
+  5,4,6,
+  5,6,7,
+  
+  1,5,7,
+  1,7,3,
+  
+  1,3,2,
+  0,1,2,
+  
+  4,0,6,
+  6,0,2,
+  
+  5,0,4,
+  1,0,5,
+  
+  7,6,2,
+  7,2,3
+};
 
-static const GLfloat kTextureCoords[] =
-{ 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0 };
 
 application_handle_t *app_handler;
 struct VideoModeAttributes video_mode;
@@ -57,9 +77,10 @@ GLuint shader_program;
 GLuint attrib_vertices;
 GLuint attrib_textureCoords;
 GLuint uniform_texture;
+GLuint uniform_mvp_mat;
 
 GLuint texture_id;
-GLuint vertex_buffers[3];
+GLuint vertex_buffers[2];
 
 double video_overlay_timestamp;
 
@@ -140,41 +161,30 @@ bool SetupTango() {
 bool SetupGraphics(int w, int h) {
   LOGI("setupGraphics(%d, %d)", w, h);
   
+  // /* Shaders */
+  // #define GL_VERTEX_PROGRAM_POINT_SIZE      0x8642
+  // #define GL_VERTEX_ATTRIB_ARRAY_NORMALIZED 0x886A
+  glEnable(0x8642);
+  
   shader_program = CreateProgram(kVertexShader, kFragmentShader);
   if (!shader_program) {
     LOGE("Could not create program.");
     return false;
   }
   
-  glEnable(GL_TEXTURE_EXTERNAL_OES);
-  glGenTextures(1, &texture_id);
-  glBindTexture(GL_TEXTURE_EXTERNAL_OES, texture_id);
-  glTexParameteri(GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-  glTexParameteri(GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-  
-  // texutre
-  uniform_texture = glGetUniformLocation(shader_program, "texture");
-  glUniform1i(uniform_texture, texture_id);
-  glBindTexture(GL_TEXTURE_EXTERNAL_OES, 0);
-  
   glGenBuffers(3, vertex_buffers);
+
   // vertice
   glBindBuffer(GL_ARRAY_BUFFER, vertex_buffers[0]);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 3 * 4, kVertices,
+  glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 3 * 8, kVertices,
                GL_STATIC_DRAW);
   glBindBuffer(GL_ARRAY_BUFFER, 0);
   
   // triangles
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vertex_buffers[1]);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLushort) * 6, kIndices,
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLushort) * 3 * 12, kIndices,
                GL_STATIC_DRAW);
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-  
-  // texture coords
-  glBindBuffer(GL_ARRAY_BUFFER, vertex_buffers[2]);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 2 * 4, kTextureCoords,
-               GL_STATIC_DRAW);
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
   
   // vertex attribute
   attrib_vertices = glGetAttribLocation(shader_program, "vertex");
@@ -184,18 +194,12 @@ bool SetupGraphics(int w, int h) {
                         (const void*) 0);
   glBindBuffer(GL_ARRAY_BUFFER, 0);
   
-  // texture coords attribute
-  attrib_textureCoords = glGetAttribLocation(shader_program, "textureCoords");
-  glBindBuffer(GL_ARRAY_BUFFER, vertex_buffers[2]);
-  glEnableVertexAttribArray(attrib_textureCoords);
-  glVertexAttribPointer(attrib_textureCoords, 2, GL_FLOAT, GL_FALSE, 0,
-                        (const void*) 0);
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
-  
   screen_width = w;
   screen_height = h;
   glViewport(0, 0, screen_width, screen_height);
   CheckGlError("glViewport");
+  
+  uniform_mvp_mat = glGetUniformLocation(shader_program, "mvp");
   
   return true;
 }
@@ -205,20 +209,30 @@ void UpdateTango()
   
 }
 
+float rotate_rate = 0.0f;
+
 bool RenderFrame() {
+  
   UpdateTango();
   
   glUseProgram(shader_program);
   CheckGlError("glUseProgram");
+  glViewport(0, 0, screen_width, screen_height);
   
   glEnable (GL_DEPTH_TEST);
   glEnable (GL_CULL_FACE);
-  glEnable (GL_BLEND);
-  glEnable(GL_TEXTURE_EXTERNAL_OES);
   
   glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
   glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
-  glViewport(0, 0, screen_width, screen_height);
+  
+  glm::mat4 Projection = glm::perspective(45.0f, (float)(screen_width / screen_height), 0.1f, 100.0f);
+  glm::mat4 View = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -5.0f));
+  
+  
+  rotate_rate+=0.02f;
+  glm::mat4 Model = glm::rotate(glm::mat4(1.0f), rotate_rate, glm::vec3(0.0f, 1.0f, 1.0f));
+  glm::mat4 MVP = Projection * View * Model;
+  glUniformMatrix4fv(uniform_mvp_mat, 1, GL_FALSE, glm::value_ptr(MVP));
   
   // vertex
   glBindBuffer(GL_ARRAY_BUFFER, vertex_buffers[0]);
@@ -227,18 +241,12 @@ bool RenderFrame() {
                         (const void*) 0);
   glBindBuffer(GL_ARRAY_BUFFER, 0);
   
-  // texture coords
-  glBindBuffer(GL_ARRAY_BUFFER, vertex_buffers[2]);
-  glEnableVertexAttribArray(attrib_textureCoords);
-  glVertexAttribPointer(attrib_textureCoords, 2, GL_FLOAT, GL_FALSE, 0,
-                        (const void*) 0);
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
-  
   // bind element array buffer
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vertex_buffers[1]);
-  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
+//  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vertex_buffers[1]);
+//  glDrawElements(GL_POINTS, 32, GL_UNSIGNED_SHORT, 0);
+  glDrawArrays(GL_POINTS, 0, 8);
   CheckGlError("glDrawElements");
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+//  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
   
   return true;
 }
@@ -246,14 +254,14 @@ bool RenderFrame() {
 #ifdef __cplusplus
 extern "C" {
 #endif
-  JNIEXPORT void JNICALL Java_com_google_tango_tangojnivideooverlay_TangoJNINative_init(
+  JNIEXPORT void JNICALL Java_com_google_tango_tangojnipointcloud_TangoJNINative_init(
                                                                                         JNIEnv * env, jobject obj, jint width, jint height)
   {
     SetupGraphics(width, height);
     SetupTango();
   }
   
-  JNIEXPORT void JNICALL Java_com_google_tango_tangojnivideooverlay_TangoJNINative_render(
+  JNIEXPORT void JNICALL Java_com_google_tango_tangojnipointcloud_TangoJNINative_render(
                                                                                           JNIEnv * env, jobject obj)
   {
     RenderFrame();
