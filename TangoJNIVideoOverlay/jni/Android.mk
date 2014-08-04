ca35e999e493905820b7fe6ec1b8193740135c91
Jason Guo <xuguo@google.com>
2014-08-04 13:43:15 -0700

    - change mk file to fit new so. - clean up c code.

--- TangoJNIVideoOverlay/jni/Android.mk
+++ TangoJNIVideoOverlay/jni/Android.mk
@@ -2,15 +2,13 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libtango-prebuilt
-#LOCAL_SRC_FILES := ../../TangoDevUpdate-0618/libs/libtango_api.so
-LOCAL_SRC_FILES := ../../TangoDevUpdate-0618/libs/libtango_service_api.so
-LOCAL_EXPORT_C_INCLUDES := ../TangoDevUpdate-0618/include
+LOCAL_SRC_FILES := ../../tango_service/libtango_api_client.so
+LOCAL_EXPORT_C_INCLUDES := ../tango_service/include
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE    := libtango-native-jni
 LOCAL_SHARED_LIBRARIES := libtango-prebuilt
-LOCAL_CFLAGS    := -Werror
-LOCAL_SRC_FILES := tango_video_overlay.cpp
-LOCAL_LDLIBS    := -llog -lGLESv2 -L$(SYSROOT)/usr/lib
+LOCAL_SRC_FILES := tango_video_overlay.c
+LOCAL_LDLIBS    := -llog -lGLESv2
 include $(BUILD_SHARED_LIBRARY)
--- TangoJNIVideoOverlay/jni/tango_video_overlay.c
+++ TangoJNIVideoOverlay/jni/tango_video_overlay.c
@@ -4,45 +4,46 @@
 #include <GLES2/gl2.h>
 #include <GLES2/gl2ext.h>
 
-#include <tango-api/application-interface.h>
-#include <tango-api/hardware-control-interface.h>
-#include <tango-api/video-overlay-interface.h>
-#include <tango-api/tango_client_api.h>
+#include <tango_client_api.h>
 
-#define  LOG_TAG    "tango_video_overlay"
-#define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
-#define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
+#define LOG_TAG "tango_video_overlay"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
 
-static const char kVertexShader[] = "precision highp float;\n"
-    "precision highp int;\n"
-    "attribute vec4 vertex;\n"
-    "attribute vec2 textureCoords;\n"
-    "varying vec2 f_textureCoords;\n"
-    "void main() {\n"
-    "  f_textureCoords = textureCoords;\n"
-    "  gl_Position = vertex;\n"
-    "}\n";
+static const char kVertexShader[] =
+  "precision highp float;\n"
+  "precision highp int;\n"
+  "attribute vec4 vertex;\n"
+  "attribute vec2 textureCoords;\n"
+  "varying vec2 f_textureCoords;\n"
+  "void main() {\n"
+  "  f_textureCoords = textureCoords;\n"
+  "  gl_Position = vertex;\n"
+  "}\n";
 
 static const char kFragmentShader[] =
-    "#extension GL_OES_EGL_image_external : require\n"
-    "precision highp float;\n"
-    "precision highp int;\n"
-    "uniform samplerExternalOES texture;\n"
-    "varying vec2 f_textureCoords;\n"
-    "void main() {\n"
-    "  gl_FragColor = texture2D(texture, f_textureCoords);\n"
-    "}\n";
+  "#extension GL_OES_EGL_image_external : require\n"
+  "precision highp float;\n"
+  "precision highp int;\n"
+  "uniform samplerExternalOES texture;\n"
+  "varying vec2 f_textureCoords;\n"
+  "void main() {\n"
+  "  gl_FragColor = texture2D(texture, f_textureCoords);\n"
+  "}\n";
 
-static const GLfloat kVertices[] = { -0.5, 0.5, 0.0, -0.5, -0.5, 0.0, 0.5, 0.5,
-    0.0, 0.5, -0.5, 0.0 };
+static const GLfloat kVertices[] =
+  { -0.5, 0.5, 0.0,
+    -0.5, -0.5, 0.0,
+    0.5, 0.5, 0.0,
+    0.5, -0.5, 0.0 };
 
-static const GLushort kIndices[] = { 0, 1, 2, 2, 1, 3 };
+static const GLushort kIndices[] =
+  { 0, 1, 2, 2, 1, 3 };
 
 static const GLfloat kTextureCoords[] =
-    { 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0 };
+  { 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0 };
 
-application_handle_t *app_handler;
-struct VideoModeAttributes video_mode;
+TangoConfig* config;
 
 GLuint screen_width;
 GLuint screen_height;
@@ -55,22 +56,9 @@ GLuint uniform_texture;
 GLuint texture_id;
 GLuint vertex_buffers[3];
 
-double video_overlay_timestamp;
-
-static void onPoseAvailable(TangoPoseData *pose) {
-  LOGI("PoseCallback: %lf %lf %lf %lf %lf\t%lf %lf %lf\n",
-         pose->timestamp,
-         pose->orientation[0],
-         pose->orientation[1],
-         pose->orientation[2],
-         pose->orientation[3],
-         pose->translation[0],
-         pose->translation[1],
-         pose->translation[2]);
-}
-
 static void CheckGlError(const char* operation) {
-  for (GLint error = glGetError(); error; error = glGetError()) {
+  GLint error = glGetError();
+  for (error; error; error = glGetError()) {
     LOGI("after %s() glError (0x%x)\n", operation, error);
   }
 }
@@ -139,7 +127,6 @@ GLuint CreateProgram(const char* vertex_source, const char* fragment_source) {
 }
 
 bool SetupTango() {
-  TangoConfig* config;
   if (TangoService_initialize() != 0) {
     LOGI("TangoService_initialize(): Failed\n");
     return -1;
@@ -157,12 +144,6 @@ bool SetupTango() {
     return -1;
   }
   
-  // Set a parameter in TangoConfig.
-  if (TangoConfig_setBool(config, "disable_opengl", true) != 0) {
-    LOGI("TangoConfig_setBool(\"disable_opengl\", true): Failed\n");
-    return -1;
-  }
-  
   // Report the current TangoConfig.
   LOGI("TangoConfig:\n%s\n", TangoConfig_toString(config));
   
@@ -172,12 +153,7 @@ bool SetupTango() {
     return -1;
   }
   
-  // Attach the onPoseAvailable callback.
-  if (TangoService_connectOnPoseAvailable(onPoseAvailable) != 0) {
-    LOGI("TangoService_connectOnPoseAvailable(): Failed\n");
-    return -1;
-  }
-  
+  // Connect texture to Tango color camera.
   TangoService_connectTextureId(TANGO_CAMERA_COLOR,
                                 texture_id);
   
@@ -202,31 +178,31 @@ bool SetupGraphics(int w, int h) {
   glTexParameteri(GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
   glTexParameteri(GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-  // texutre
+  // Assign texutre buufer.
   uniform_texture = glGetUniformLocation(shader_program, "texture");
   glUniform1i(uniform_texture, texture_id);
   glBindTexture(GL_TEXTURE_EXTERNAL_OES, 0);
 
   glGenBuffers(3, vertex_buffers);
-  // vertice
+  // Allocate vertices buffer.
   glBindBuffer(GL_ARRAY_BUFFER, vertex_buffers[0]);
   glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 3 * 4, kVertices,
                GL_STATIC_DRAW);
   glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-  // triangles
+  // Allocate triangle indices buffer.
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vertex_buffers[1]);
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLushort) * 6, kIndices,
                GL_STATIC_DRAW);
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
-  // texture coords
+  // Allocate texture coordinates buufer.
   glBindBuffer(GL_ARRAY_BUFFER, vertex_buffers[2]);
   glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 2 * 4, kTextureCoords,
                GL_STATIC_DRAW);
   glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-  // vertex attribute
+  // Assign the vertices attribute data.
   attrib_vertices = glGetAttribLocation(shader_program, "vertex");
   glBindBuffer(GL_ARRAY_BUFFER, vertex_buffers[0]);
   glEnableVertexAttribArray(attrib_vertices);
@@ -234,7 +210,7 @@ bool SetupGraphics(int w, int h) {
                         (const void*) 0);
   glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-  // texture coords attribute
+  // Assign the texture coordinates attribute data.
   attrib_textureCoords = glGetAttribLocation(shader_program, "textureCoords");
   glBindBuffer(GL_ARRAY_BUFFER, vertex_buffers[2]);
   glEnableVertexAttribArray(attrib_textureCoords);
@@ -252,12 +228,11 @@ bool SetupGraphics(int w, int h) {
 
 void UpdateTango()
 {
+  // Update Tango color camera's texture.
   TangoService_updateTexture(TANGO_CAMERA_COLOR);
 }
 
 bool RenderFrame() {
-  UpdateTango();
-
   glUseProgram(shader_program);
   CheckGlError("glUseProgram");
   
@@ -270,21 +245,21 @@ bool RenderFrame() {
   glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
   glViewport(0, 0, screen_width, screen_height);
 
-  // vertex
+  // Bind vertices buffer.
   glBindBuffer(GL_ARRAY_BUFFER, vertex_buffers[0]);
   glEnableVertexAttribArray(attrib_vertices);
   glVertexAttribPointer(attrib_vertices, 3, GL_FLOAT, GL_FALSE, 0,
                         (const void*) 0);
   glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-  // texture coords
+  // Bind texture coordinates buffer.
   glBindBuffer(GL_ARRAY_BUFFER, vertex_buffers[2]);
   glEnableVertexAttribArray(attrib_textureCoords);
   glVertexAttribPointer(attrib_textureCoords, 2, GL_FLOAT, GL_FALSE, 0,
                         (const void*) 0);
   glBindBuffer(GL_ARRAY_BUFFER, 0);
   
-  // bind element array buffer
+  // Bind element array buffer.
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vertex_buffers[1]);
   glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
   CheckGlError("glDrawElements");
@@ -293,21 +268,16 @@ bool RenderFrame() {
   return true;
 }
 
-#ifdef __cplusplus
-extern "C" {
-#endif
 JNIEXPORT void JNICALL Java_com_google_tango_tangojnivideooverlay_TangoJNINative_init(
     JNIEnv * env, jobject obj, jint width, jint height)
 {
-  SetupGraphics(width, height);
   SetupTango();
+  SetupGraphics(width, height);
 }
 
 JNIEXPORT void JNICALL Java_com_google_tango_tangojnivideooverlay_TangoJNINative_render(
     JNIEnv * env, jobject obj)
 {
+  UpdateTango();
   RenderFrame();
 }
-#ifdef __cplusplus
-}
-#endif
