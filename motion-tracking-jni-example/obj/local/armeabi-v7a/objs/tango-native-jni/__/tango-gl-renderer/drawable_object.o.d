b8013498800051cb1a7e2f5effded8a62d8b9583
Cheng Yang <chengyangorange@gmail.com>
2014-08-25 22:55:40 -0700

    Naming convention.

---
+++ motion-tracking-jni-example/obj/local/armeabi-v7a/objs/tango-native-jni/__/tango-gl-renderer/drawable_object.o.d
@@ -0,0 +1,496 @@
+obj/local/armeabi-v7a/objs/tango-native-jni/__/tango-gl-renderer/drawable_object.o: \
+ jni/../../tango-gl-renderer/drawable_object.cpp \
+ jni/../../tango-gl-renderer/drawable_object.h \
+ jni/../../tango-gl-renderer/gl_util.h \
+ /Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/stdlib.h \
+ /Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/cdefs.h \
+ /Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/android/api-level.h \
+ /Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/string.h \
+ /Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/malloc.h \
+ /Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/alloca.h \
+ /Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/strings.h \
+ /Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/types.h \
+ /Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/stdint.h \
+ /Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/_types.h \
+ /Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/machine/_types.h \
+ /Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/linux/posix_types.h \
+ /Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/linux/stddef.h \
+ /Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/linux/compiler.h \
+ /Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/asm/posix_types.h \
+ /Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/asm/types.h \
+ /Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/linux/types.h \
+ /Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/machine/kernel.h \
+ /Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/memory.h \
+ /Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/jni.h \
+ /Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/android/log.h \
+ /Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/GLES2/gl2.h \
+ /Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/GLES2/gl2platform.h \
+ /Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/KHR/khrplatform.h \
+ /Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/GLES2/gl2ext.h \
+ ../third-party/glm/glm/glm.hpp ../third-party/glm/glm/detail/_fixes.hpp \
+ /Users/chengyang/Downloads/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath \
+ /Users/chengyang/Downloads/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/climits \
+ /Users/chengyang/Downloads/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cfloat \
+ /Users/chengyang/Downloads/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/limits \
+ /Users/chengyang/Downloads/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cassert \
+ ../third-party/glm/glm/fwd.hpp \
+ ../third-party/glm/glm/detail/type_int.hpp \
+ ../third-party/glm/glm/detail/setup.hpp \
+ /Users/chengyang/Downloads/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cstdint \
+ ../third-party/glm/glm/detail/type_float.hpp \
+ ../third-party/glm/glm/detail/type_vec.hpp \
+ ../third-party/glm/glm/detail/precision.hpp \
+ ../third-party/glm/glm/detail/type_int.hpp \
+ ../third-party/glm/glm/detail/type_mat.hpp \
+ ../third-party/glm/glm/vec2.hpp \
+ ../third-party/glm/glm/detail/type_vec2.hpp \
+ ../third-party/glm/glm/detail/type_vec.hpp \
+ /Users/chengyang/Downloads/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef \
+ ../third-party/glm/glm/detail/type_vec2.inl \
+ ../third-party/glm/glm/vec3.hpp \
+ ../third-party/glm/glm/detail/type_vec3.hpp \
+ ../third-party/glm/glm/detail/type_vec3.inl \
+ ../third-party/glm/glm/vec4.hpp \
+ ../third-party/glm/glm/detail/type_vec4.hpp \
+ ../third-party/glm/glm/detail/type_vec4.inl \
+ ../third-party/glm/glm/mat2x2.hpp \
+ ../third-party/glm/glm/detail/type_mat2x2.hpp \
+ ../third-party/glm/glm/detail/../fwd.hpp \
+ ../third-party/glm/glm/detail/type_vec2.hpp \
+ ../third-party/glm/glm/detail/type_mat.hpp \
+ ../third-party/glm/glm/detail/type_mat2x2.inl \
+ ../third-party/glm/glm/mat2x3.hpp \
+ ../third-party/glm/glm/detail/type_mat2x3.hpp \
+ ../third-party/glm/glm/detail/type_vec3.hpp \
+ ../third-party/glm/glm/detail/type_mat2x3.inl \
+ ../third-party/glm/glm/mat2x4.hpp \
+ ../third-party/glm/glm/detail/type_mat2x4.hpp \
+ ../third-party/glm/glm/detail/type_vec4.hpp \
+ ../third-party/glm/glm/detail/type_mat2x4.inl \
+ ../third-party/glm/glm/mat3x2.hpp \
+ ../third-party/glm/glm/detail/type_mat3x2.hpp \
+ ../third-party/glm/glm/detail/type_mat3x2.inl \
+ ../third-party/glm/glm/mat3x3.hpp \
+ ../third-party/glm/glm/detail/type_mat3x3.hpp \
+ ../third-party/glm/glm/detail/type_mat3x3.inl \
+ ../third-party/glm/glm/mat3x4.hpp \
+ ../third-party/glm/glm/detail/type_mat3x4.hpp \
+ ../third-party/glm/glm/detail/type_mat3x4.inl \
+ ../third-party/glm/glm/mat4x2.hpp \
+ ../third-party/glm/glm/detail/type_mat4x2.hpp \
+ ../third-party/glm/glm/detail/type_mat4x2.inl \
+ ../third-party/glm/glm/mat4x3.hpp \
+ ../third-party/glm/glm/detail/type_mat4x3.hpp \
+ ../third-party/glm/glm/detail/type_mat4x3.inl \
+ ../third-party/glm/glm/mat4x4.hpp \
+ ../third-party/glm/glm/detail/type_mat4x4.hpp \
+ ../third-party/glm/glm/detail/type_mat4x4.inl \
+ ../third-party/glm/glm/trigonometric.hpp \
+ ../third-party/glm/glm/detail/func_trigonometric.hpp \
+ ../third-party/glm/glm/detail/func_trigonometric.inl \
+ ../third-party/glm/glm/detail/_vectorize.hpp \
+ ../third-party/glm/glm/detail/type_vec1.hpp \
+ ../third-party/glm/glm/detail/type_vec1.inl \
+ ../third-party/glm/glm/exponential.hpp \
+ ../third-party/glm/glm/detail/func_exponential.hpp \
+ ../third-party/glm/glm/detail/func_exponential.inl \
+ ../third-party/glm/glm/detail/func_vector_relational.hpp \
+ ../third-party/glm/glm/detail/func_vector_relational.inl \
+ ../third-party/glm/glm/common.hpp \
+ ../third-party/glm/glm/detail/func_common.hpp \
+ ../third-party/glm/glm/detail/_fixes.hpp \
+ ../third-party/glm/glm/detail/func_common.inl \
+ ../third-party/glm/glm/packing.hpp \
+ ../third-party/glm/glm/detail/func_packing.hpp \
+ ../third-party/glm/glm/detail/func_packing.inl \
+ ../third-party/glm/glm/detail/func_common.hpp \
+ ../third-party/glm/glm/detail/type_half.hpp \
+ ../third-party/glm/glm/detail/type_half.inl \
+ ../third-party/glm/glm/geometric.hpp \
+ ../third-party/glm/glm/detail/func_geometric.hpp \
+ ../third-party/glm/glm/detail/func_geometric.inl \
+ ../third-party/glm/glm/detail/func_exponential.hpp \
+ ../third-party/glm/glm/detail/type_float.hpp \
+ ../third-party/glm/glm/matrix.hpp \
+ ../third-party/glm/glm/detail/func_matrix.hpp \
+ ../third-party/glm/glm/detail/../detail/precision.hpp \
+ ../third-party/glm/glm/detail/../detail/setup.hpp \
+ ../third-party/glm/glm/detail/../detail/type_mat.hpp \
+ ../third-party/glm/glm/detail/../vec2.hpp \
+ ../third-party/glm/glm/detail/../vec3.hpp \
+ ../third-party/glm/glm/detail/../vec4.hpp \
+ ../third-party/glm/glm/detail/../mat2x2.hpp \
+ ../third-party/glm/glm/detail/../mat2x3.hpp \
+ ../third-party/glm/glm/detail/../mat2x4.hpp \
+ ../third-party/glm/glm/detail/../mat3x2.hpp \
+ ../third-party/glm/glm/detail/../mat3x3.hpp \
+ ../third-party/glm/glm/detail/../mat3x4.hpp \
+ ../third-party/glm/glm/detail/../mat4x2.hpp \
+ ../third-party/glm/glm/detail/../mat4x3.hpp \
+ ../third-party/glm/glm/detail/../mat4x4.hpp \
+ ../third-party/glm/glm/detail/func_matrix.inl \
+ ../third-party/glm/glm/detail/../geometric.hpp \
+ ../third-party/glm/glm/vector_relational.hpp \
+ ../third-party/glm/glm/detail/func_vector_relational.hpp \
+ ../third-party/glm/glm/integer.hpp \
+ ../third-party/glm/glm/detail/func_integer.hpp \
+ ../third-party/glm/glm/detail/func_integer.inl \
+ ../third-party/glm/glm/gtc/matrix_transform.hpp \
+ ../third-party/glm/glm/gtc/../mat4x4.hpp \
+ ../third-party/glm/glm/gtc/../vec2.hpp \
+ ../third-party/glm/glm/gtc/../vec3.hpp \
+ ../third-party/glm/glm/gtc/../vec4.hpp \
+ ../third-party/glm/glm/gtc/../gtc/constants.hpp \
+ ../third-party/glm/glm/gtc/../gtc/../detail/setup.hpp \
+ ../third-party/glm/glm/gtc/../gtc/constants.inl \
+ ../third-party/glm/glm/gtc/matrix_transform.inl \
+ ../third-party/glm/glm/gtc/../geometric.hpp \
+ ../third-party/glm/glm/gtc/../trigonometric.hpp \
+ ../third-party/glm/glm/gtc/../matrix.hpp \
+ ../third-party/glm/glm/gtc/quaternion.hpp \
+ ../third-party/glm/glm/gtc/../mat3x3.hpp \
+ ../third-party/glm/glm/gtc/quaternion.inl \
+ ../third-party/glm/glm/gtc/../exponential.hpp \
+ ../third-party/glm/glm/gtc/type_ptr.hpp \
+ ../third-party/glm/glm/gtc/../gtc/quaternion.hpp \
+ ../third-party/glm/glm/gtc/../mat2x2.hpp \
+ ../third-party/glm/glm/gtc/../mat2x3.hpp \
+ ../third-party/glm/glm/gtc/../mat2x4.hpp \
+ ../third-party/glm/glm/gtc/../mat3x2.hpp \
+ ../third-party/glm/glm/gtc/../mat3x4.hpp \
+ ../third-party/glm/glm/gtc/../mat4x2.hpp \
+ ../third-party/glm/glm/gtc/../mat4x3.hpp \
+ /Users/chengyang/Downloads/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cstring \
+ ../third-party/glm/glm/gtc/type_ptr.inl
+
+jni/../../tango-gl-renderer/drawable_object.h:
+
+jni/../../tango-gl-renderer/gl_util.h:
+
+/Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/stdlib.h:
+
+/Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/android/api-level.h:
+
+/Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/string.h:
+
+/Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/malloc.h:
+
+/Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/alloca.h:
+
+/Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/strings.h:
+
+/Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/types.h:
+
+/Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/stdint.h:
+
+/Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/_types.h:
+
+/Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/machine/_types.h:
+
+/Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/linux/stddef.h:
+
+/Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/linux/compiler.h:
+
+/Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/asm/types.h:
+
+/Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/linux/types.h:
+
+/Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/machine/kernel.h:
+
+/Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/memory.h:
+
+/Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/jni.h:
+
+/Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/android/log.h:
+
+/Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/GLES2/gl2.h:
+
+/Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/GLES2/gl2platform.h:
+
+/Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/KHR/khrplatform.h:
+
+/Users/chengyang/Downloads/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/GLES2/gl2ext.h:
+
+../third-party/glm/glm/glm.hpp:
+
+../third-party/glm/glm/detail/_fixes.hpp:
+
+/Users/chengyang/Downloads/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath:
+
+/Users/chengyang/Downloads/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/climits:
+
+/Users/chengyang/Downloads/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cfloat:
+
+/Users/chengyang/Downloads/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/limits:
+
+/Users/chengyang/Downloads/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cassert:
+
+../third-party/glm/glm/fwd.hpp:
+
+../third-party/glm/glm/detail/type_int.hpp:
+
+../third-party/glm/glm/detail/setup.hpp:
+
+/Users/chengyang/Downloads/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cstdint:
+
+../third-party/glm/glm/detail/type_float.hpp:
+
+../third-party/glm/glm/detail/type_vec.hpp:
+
+../third-party/glm/glm/detail/precision.hpp:
+
+../third-party/glm/glm/detail/type_int.hpp:
+
+../third-party/glm/glm/detail/type_mat.hpp:
+
+../third-party/glm/glm/vec2.hpp:
+
+../third-party/glm/glm/detail/type_vec2.hpp:
+
+../third-party/glm/glm/detail/type_vec.hpp:
+
+/Users/chengyang/Downloads/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef:
+
+../third-party/glm/glm/detail/type_vec2.inl:
+
+../third-party/glm/glm/vec3.hpp:
+
+../third-party/glm/glm/detail/type_vec3.hpp:
+
+../third-party/glm/glm/detail/type_vec3.inl:
+
+../third-party/glm/glm/vec4.hpp:
+
+../third-party/glm/glm/detail/type_vec4.hpp:
+
+../third-party/glm/glm/detail/type_vec4.inl:
+
+../third-party/glm/glm/mat2x2.hpp:
+
+../third-party/glm/glm/detail/type_mat2x2.hpp:
+
+../third-party/glm/glm/detail/../fwd.hpp:
+
+../third-party/glm/glm/detail/type_vec2.hpp:
+
+../third-party/glm/glm/detail/type_mat.hpp:
+
+../third-party/glm/glm/detail/type_mat2x2.inl:
+
+../third-party/glm/glm/mat2x3.hpp:
+
+../third-party/glm/glm/detail/type_mat2x3.hpp:
+
+../third-party/glm/glm/detail/type_vec3.hpp:
+
+../third-party/glm/glm/detail/type_mat2x3.inl:
+
+../third-party/glm/glm/mat2x4.hpp:
+
+../third-party/glm/glm/detail/type_mat2x4.hpp:
+
+../third-party/glm/glm/detail/type_vec4.hpp:
+
+../third-party/glm/glm/detail/type_mat2x4.inl:
+
+../third-party/glm/glm/mat3x2.hpp:
+
+../third-party/glm/glm/detail/type_mat3x2.hpp:
+
+../third-party/glm/glm/detail/type_mat3x2.inl:
+
+../third-party/glm/glm/mat3x3.hpp:
+
+../third-party/glm/glm/detail/type_mat3x3.hpp:
+
+../third-party/glm/glm/detail/type_mat3x3.inl:
+
+../third-party/glm/glm/mat3x4.hpp:
+
+../third-party/glm/glm/detail/type_mat3x4.hpp:
+
+../third-party/glm/glm/detail/type_mat3x4.inl:
+
+../third-party/glm/glm/mat4x2.hpp:
+
+../third-party/glm/glm/detail/type_mat4x2.hpp:
+
+../third-party/glm/glm/detail/type_mat4x2.inl:
+
+../third-party/glm/glm/mat4x3.hpp:
+
+../third-party/glm/glm/detail/type_mat4x3.hpp:
+
+../third-party/glm/glm/detail/type_mat4x3.inl:
+
+../third-party/glm/glm/mat4x4.hpp:
+
+../third-party/glm/glm/detail/type_mat4x4.hpp:
+
+../third-party/glm/glm/detail/type_mat4x4.inl:
+
+../third-party/glm/glm/trigonometric.hpp:
+
+../third-party/glm/glm/detail/func_trigonometric.hpp:
+
+../third-party/glm/glm/detail/func_trigonometric.inl:
+
+../third-party/glm/glm/detail/_vectorize.hpp:
+
+../third-party/glm/glm/detail/type_vec1.hpp:
+
+../third-party/glm/glm/detail/type_vec1.inl:
+
+../third-party/glm/glm/exponential.hpp:
+
+../third-party/glm/glm/detail/func_exponential.hpp:
+
+../third-party/glm/glm/detail/func_exponential.inl:
+
+../third-party/glm/glm/detail/func_vector_relational.hpp:
+
+../third-party/glm/glm/detail/func_vector_relational.inl:
+
+../third-party/glm/glm/common.hpp:
+
+../third-party/glm/glm/detail/func_common.hpp:
+
+../third-party/glm/glm/detail/_fixes.hpp:
+
+../third-party/glm/glm/detail/func_common.inl:
+
+../third-party/glm/glm/packing.hpp:
+
+../third-party/glm/glm/detail/func_packing.hpp:
+
+../third-party/glm/glm/detail/func_packing.inl:
+
+../third-party/glm/glm/detail/func_common.hpp:
+
+../third-party/glm/glm/detail/type_half.hpp:
+
+../third-party/glm/glm/detail/type_half.inl:
+
+../third-party/glm/glm/geometric.hpp:
+
+../third-party/glm/glm/detail/func_geometric.hpp:
+
+../third-party/glm/glm/detail/func_geometric.inl:
+
+../third-party/glm/glm/detail/func_exponential.hpp:
+
+../third-party/glm/glm/detail/type_float.hpp:
+
+../third-party/glm/glm/matrix.hpp:
+
+../third-party/glm/glm/detail/func_matrix.hpp:
+
+../third-party/glm/glm/detail/../detail/precision.hpp:
+
+../third-party/glm/glm/detail/../detail/setup.hpp:
+
+../third-party/glm/glm/detail/../detail/type_mat.hpp:
+
+../third-party/glm/glm/detail/../vec2.hpp:
+
+../third-party/glm/glm/detail/../vec3.hpp:
+
+../third-party/glm/glm/detail/../vec4.hpp:
+
+../third-party/glm/glm/detail/../mat2x2.hpp:
+
+../third-party/glm/glm/detail/../mat2x3.hpp:
+
+../third-party/glm/glm/detail/../mat2x4.hpp:
+
+../third-party/glm/glm/detail/../mat3x2.hpp:
+
+../third-party/glm/glm/detail/../mat3x3.hpp:
+
+../third-party/glm/glm/detail/../mat3x4.hpp:
+
+../third-party/glm/glm/detail/../mat4x2.hpp:
+
+../third-party/glm/glm/detail/../mat4x3.hpp:
+
+../third-party/glm/glm/detail/../mat4x4.hpp:
+
+../third-party/glm/glm/detail/func_matrix.inl:
+
+../third-party/glm/glm/detail/../geometric.hpp:
+
+../third-party/glm/glm/vector_relational.hpp:
+
+../third-party/glm/glm/detail/func_vector_relational.hpp:
+
+../third-party/glm/glm/integer.hpp:
+
+../third-party/glm/glm/detail/func_integer.hpp:
+
+../third-party/glm/glm/detail/func_integer.inl:
+
+../third-party/glm/glm/gtc/matrix_transform.hpp:
+
+../third-party/glm/glm/gtc/../mat4x4.hpp:
+
+../third-party/glm/glm/gtc/../vec2.hpp:
+
+../third-party/glm/glm/gtc/../vec3.hpp:
+
+../third-party/glm/glm/gtc/../vec4.hpp:
+
+../third-party/glm/glm/gtc/../gtc/constants.hpp:
+
+../third-party/glm/glm/gtc/../gtc/../detail/setup.hpp:
+
+../third-party/glm/glm/gtc/../gtc/constants.inl:
+
+../third-party/glm/glm/gtc/matrix_transform.inl:
+
+../third-party/glm/glm/gtc/../geometric.hpp:
+
+../third-party/glm/glm/gtc/../trigonometric.hpp:
+
+../third-party/glm/glm/gtc/../matrix.hpp:
+
+../third-party/glm/glm/gtc/quaternion.hpp:
+
+../third-party/glm/glm/gtc/../mat3x3.hpp:
+
+../third-party/glm/glm/gtc/quaternion.inl:
+
+../third-party/glm/glm/gtc/../exponential.hpp:
+
+../third-party/glm/glm/gtc/type_ptr.hpp:
+
+../third-party/glm/glm/gtc/../gtc/quaternion.hpp:
+
+../third-party/glm/glm/gtc/../mat2x2.hpp:
+
+../third-party/glm/glm/gtc/../mat2x3.hpp:
+
+../third-party/glm/glm/gtc/../mat2x4.hpp:
+
+../third-party/glm/glm/gtc/../mat3x2.hpp:
+
+../third-party/glm/glm/gtc/../mat3x4.hpp:
+
+../third-party/glm/glm/gtc/../mat4x2.hpp:
+
+../third-party/glm/glm/gtc/../mat4x3.hpp:
+
+/Users/chengyang/Downloads/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cstring:
+
+../third-party/glm/glm/gtc/type_ptr.inl:
