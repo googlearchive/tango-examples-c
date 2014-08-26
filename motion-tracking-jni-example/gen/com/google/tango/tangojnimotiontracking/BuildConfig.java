83b1fdbc5ff43fe0434033802f82560c5ea76757
Jason Guo <xuguo@google.com>
2014-08-26 09:46:13 -0700

    - clean up obj, bin, etc.. folders

--- motion-tracking-jni-example/gen/com/google/tango/tangojnimotiontracking/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.google.tango.tangojnimotiontracking;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
