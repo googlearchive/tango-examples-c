6aafcd9ac53f96942893859b14271ae6b124c77a
Jason Guo <xuguo@google.com>
2014-09-02 19:21:51 -0700

    - make full-screen.

--- video-overlay-jni-example-experimental/jni/tango_video_overlay.c
+++ video-overlay-jni-example-experimental/jni/tango_video_overlay.c
@@ -32,10 +32,10 @@ static const char kFragmentShader[] =
   "}\n";
 
 static const GLfloat kVertices[] =
-  { -0.5, 0.5, 0.0,
-    -0.5, -0.5, 0.0,
-    0.5, 0.5, 0.0,
-    0.5, -0.5, 0.0 };
+  { -1.0, 1.0, 0.0,
+    -1.0, -1.0, 0.0,
+    1.0, 1.0, 0.0,
+    1.0, -1.0, 0.0 };
 
 static const GLushort kIndices[] =
   { 0, 1, 2, 2, 1, 3 };
