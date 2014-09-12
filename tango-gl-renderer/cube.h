3ecfd745a402bf1d1e9546a5a05acfd56faf9d6d
Cheng Yang <chengyangorange@gmail.com>
2014-09-12 11:38:53 -0700

    Adding AR marker and video overlay.

---
+++ tango-gl-renderer/cube.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2014 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CUBE_H
+#define CUBE_H
+
+#include "drawable_object.h"
+#include "gl_util.h"
+
+class Cube : public DrawableObject {
+ public:
+  Cube();
+  void Render(glm::mat4 projection_mat, glm::mat4 view_mat);
+ private:
+  GLuint vertex_buffer_;
+  GLuint shader_program_;
+  GLuint attrib_vertices_;
+  GLuint uniform_mvp_mat_;
+};
+
+#endif  // CUBE_H
