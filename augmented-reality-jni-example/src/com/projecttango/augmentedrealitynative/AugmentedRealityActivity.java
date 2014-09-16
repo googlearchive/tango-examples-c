4e5038b247fdb18759e9dc8be9a95aba45e1f002
Cheng Yang <chengyangorange@gmail.com>
2014-09-16 13:15:40 -0700

    Immersive mode enabled.

--- augmented-reality-jni-example/src/com/projecttango/augmentedrealitynative/AugmentedRealityActivity.java
+++ augmented-reality-jni-example/src/com/projecttango/augmentedrealitynative/AugmentedRealityActivity.java
@@ -49,9 +49,13 @@ public class AugmentedRealityActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		getWindow().getDecorView().setSystemUiVisibility(
+				View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+	            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+	            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+	            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+	            | View.SYSTEM_UI_FLAG_FULLSCREEN
+	            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 		
 		setContentView(R.layout.activity_augmented_reality);
 
