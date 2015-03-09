3d9a10952bd059a411bd74f5a23ef4c7c6978e84
Jason Guo <xuguo@google.com>
2015-03-09 15:12:46 -0700

    - drop start jni example

---
+++ starter-jni-example/app/src/main/java/com/projecttango/experiments/nativemotiontracking/TangoJNINative.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2014 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.projecttango.experiments.nativemotiontracking;
+
+/**
+ * Interfaces between C and Java.
+ */
+public class TangoJNINative {
+    static {
+        System.loadLibrary("motion_tracking_jni_example");
+    }
+
+    public static native int tangoInitialize(MotionTrackingActivity activity);
+
+    public static native void tangoSetupConfig();
+
+    public static native int tangoConnect();
+
+    public static native int tangoConnectCallbacks();
+
+    public static native void tangoDisconnect();
+
+    public static native void freeGLContent();
+
+    public static native void setupGraphic(int width, int height);
+
+    public static native void render();
+}
