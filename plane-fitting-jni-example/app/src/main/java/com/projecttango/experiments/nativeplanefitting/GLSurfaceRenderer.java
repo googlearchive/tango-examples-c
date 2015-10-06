b72b89175c5511b073b323ecd54b9fb91423ae62
Jason Guo <xuguo@google.com>
2015-10-06 11:24:51 -0700

    add plane fitting example, add support library

---
+++ plane-fitting-jni-example/app/src/main/java/com/projecttango/experiments/nativeplanefitting/GLSurfaceRenderer.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.projecttango.experiments.nativeplanefitting;
+
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Renders graphic content.
+ */
+public class GLSurfaceRenderer implements GLSurfaceView.Renderer {
+    private MainActivity mMainActivity;
+
+    public GLSurfaceRenderer(MainActivity mainActivity) {
+        mMainActivity = mainActivity;
+    }
+
+    // Render loop of the GL context.
+    public void onDrawFrame(GL10 gl) {
+        JNIInterface.render();
+    }
+
+    // Called when the surface size changes.
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        JNIInterface.setViewPort(width, height);
+    }
+
+    // Called when the surface is created or recreated.
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        mMainActivity.surfaceCreated();
+    }
+}
